bb8299009ac51458ccfa026ff344bb8da076e30a
Sap.Dev <rizkysapp01@gmail.com>
2025-08-08 16:16:07 +0700

    style: adjust responsive font size and navbar padding
    Reduce h1 font size on mobile for better readability and decrease navbar padding on medium screens for improved spacing

--- src/components/Layouts/navbar/Navbar.tsx
+++ src/components/Layouts/navbar/Navbar.tsx
@@ -11,7 +11,7 @@ const Navbar = () => {
   const isPhone = useIsPhoneBreakpoint();
 
   return (
-    <div className="md:py-3 py-2 h-[15%] md:h-[30%] ">
+    <div className="md:py-1 py-2 h-[15%] md:h-[30%] ">
       <div className="flex flex-col space-y-2 md:space-y-5 lg:space-y-10">
         <Logo className="" />
         <Container>{isPhone ? <NavMenuIsPhone /> : <NavMenu />}</Container>
