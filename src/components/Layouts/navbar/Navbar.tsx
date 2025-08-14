7f0ab59279acf3dba8af7d5204d53642446a1c70
Sap.Dev <rizkysapp01@gmail.com>
2025-08-14 09:46:16 +0700

    Style: Update basePath in next.config.ts and adjust Navbar padding
    - Set basePath to "/crytobot" in next.config.ts for routing configuration
    - Increase vertical padding in Navbar component for improved layout

--- src/components/Layouts/navbar/Navbar.tsx
+++ src/components/Layouts/navbar/Navbar.tsx
@@ -11,7 +11,7 @@ const Navbar = () => {
   const isPhone = useIsPhoneBreakpoint();
 
   return (
-    <div className="md:py-1 py-2 h-[10%] md:h-[30%]">
+    <div className="md:py-1 py-5 h-[10%] md:h-[30%]">
       <div className="flex md:flex-col space-y-0 md:space-y-5 lg:space-y-10 relative py-2 md:py-0">
         <Logo className="" />
         <Container>{isPhone ? <NavMenuIsPhone /> : <NavMenu />}</Container>
